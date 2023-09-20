3055f37d1e020fce9ad36ae3033e52f7fe977bd6
Matias Lombardi <matiiaslombardi@gmail.com>
2023-09-20 10:05:42 -0300

    V.1.3.4 - Fixed build deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    outDir: 'docs', // For deploy on GHPages
+    vite: {
+        build: {
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'entry.[hash].js',
+                    chunkFileNames: 'chunks/chunk.[hash].js',
+                    assetFileNames: 'assets/asset.[hash][extname]',
+                },
+            },
+        },
+    }
+});
