08d1c41d3a5abb11e5109e38ab812e51d4fd623e
Lombardimn <matiiaslombardi@gmail.com>
2024-04-19 12:14:20 -0300

    GitActions configure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+// Configurations access routes
+
+const SERVER_PORT = 4321
+const LOCAL_POST = `https://localhost:${SERVER_PORT}` 
+const SITE_URL = 'https://github.com/lombardidev.github.io'
+const URL_DOMAIN = 'https://lombardidev.ar'
+// const SITE_URL = LOCAL_POST
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: SITE_URL,
 });
