860aa5a7cac0da4fdc5eda21c5a77f36583ea921
Lombardimn <matiiaslombardi@gmail.com>
2024-04-23 15:49:10 -0300

    configuration astroconfig v.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // Configurations access routes
 
 const SERVER_PORT = 4321
-const SITE_URL = 'https://github.com/lombardidev.github.io'
+const SITE_URL = 'https://github.com/lombardidev.github.io/'
 //const LOCAL_POST = `https://localhost:${SERVER_PORT}` 
 //const URL_DOMAIN = 'https://lombardidev.ar'
 // const SITE_URL = LOCAL_POST
